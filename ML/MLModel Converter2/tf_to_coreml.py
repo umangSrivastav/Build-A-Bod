90cc86a3e852997a5d70d2d3609c762183e300eb
Aditya Ramkumar <ramkumar.aditya@gmail.com>
2019-02-10 01:19:23 -0800

    Trying a different mlmodel

---
+++ ML/MLModel Converter2/tf_to_coreml.py
@@ -0,0 +1,12 @@
+import tfcoreml
+tfcoreml.convert(tf_model_path='../model/tf_files/retrained_graph.pb',
+        mlmodel_path='output.mlmodel',
+        input_name_shape_dict={"Mul:0":[1,224,224,3]},
+        output_feature_names=['final_result:0'],
+        image_input_names = 'input:0',
+        class_labels = 'retrained_labels.txt',
+        red_bias = -1,
+        green_bias = -1,
+        blue_bias = -1,
+        image_scale = 2.0/255.0
+)
