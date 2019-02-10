c8b17b16af3687c1c3627832b0f9955fcb7d9cdb
Aditya Ramkumar <ramkumar.aditya@gmail.com>
2019-02-10 00:56:11 -0800

    Updated mlmodel

--- ML/MLModel Converter/script.py
+++ ML/MLModel Converter/script.py
@@ -29,7 +29,7 @@ with tf.Graph().as_default() as g:
 # Model Shape
 input_tensor_shapes = {"input:0":[1,224,224,3], "MobilenetV1/Conv2d_0/weights:0":[3, 3, 3, 16], "MobilenetV1/Conv2d_0/weights:0":[3, 3, 3, 16], "MobilenetV1/Conv2d_0/weights/read:0":[3, 3, 3, 16], "final_training_ops/weights/final_weights/read:0":[1001, 2], "final_training_ops/biases/final_biases/read:0":[2, 1]} 
 # Input Name
-image_input_name = ['input:0']
+image_input_name = ['input:0', 'MobilenetV1/Conv2d_0/weights:0', 'MobilenetV1/Conv2d_0/weights:0', 'MobilenetV1/Conv2d_0/weights/read:0', 'MobilenetV1/Conv2d_0/weights/read:0', 'final_training_ops/weights/final_weights/read:0', 'final_training_ops/biases/final_biases/read:0']
 # Output CoreML model path
 coreml_model_file = './myModel.mlmodel'
 # Output name
@@ -45,4 +45,33 @@ coreml_model = tfcoreml.convert(
         output_feature_names=output_tensor_names,
         image_input_names = image_input_name,
         class_labels = class_labels)
+
+
+# Get image pre-processing parameters of a saved CoreML model
+spec = coremltools.models.utils.load_spec(coreml_model_file)
+if spec.WhichOneof('Type') == 'neuralNetworkClassifier':
+  nn = spec.neuralNetworkClassifier
+if spec.WhichOneof('Type') == 'neuralNetwork':
+  nn = spec.neuralNetwork  
+if spec.WhichOneof('Type') == 'neuralNetworkRegressor':
+  nn = spec.neuralNetworkRegressor
+
+preprocessing = nn.preprocessing[0].scaler
+
+inp = spec.description.input[0]
+if inp.type.WhichOneof('Type') == 'imageType':
+  colorspace = _FeatureTypes_pb2.ImageFeatureType.ColorSpace.Name(inp.type.imageType.colorSpace)
+
+
+coreml_model = tfcoreml.convert(
+        tf_model_path=tf_model_path,
+        mlmodel_path=coreml_model_file,
+        input_name_shape_dict=input_tensor_shapes,
+        output_feature_names=output_tensor_names,
+        image_input_names = image_input_name,
+        class_labels = class_labels,
+        red_bias = -1,
+        green_bias = -1,
+        blue_bias = -1,
+        image_scale = 2.0/255.0)
 
