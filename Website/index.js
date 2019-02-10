9772493aa6f396539e64b2995d1ae326ff0c8dd1
Maxwell Chen <maxhchen@gmail.com>
2019-02-10 03:22:52 -0800

    Link out to GitHub

--- Website/index.js
+++ Website/index.js
@@ -8,6 +8,8 @@ function scrollto(div)
 
 let backgroundCircles = document.querySelectorAll('.circle');
 let arrows = document.querySelectorAll('.fa');
+let webLinks = document.querySelectorAll('#webLink');
+console.log(webLinks);
 
 backgroundCircles.forEach(circle => {
   function growCircle(scaleFactor, color) {
@@ -19,7 +21,7 @@ backgroundCircles.forEach(circle => {
       delay: function(circle, i, l) {
         return i * 100;
       }
-      });
+    });
   }
 
   function enterButton() {
@@ -27,14 +29,14 @@ backgroundCircles.forEach(circle => {
       targets: circle,
       backgroundColor: '#A3A9AA',
       scale: 2.1
-    })
+    });
   }
   function leaveButton() {
     anime({
       targets: circle,
       backgroundColor: '#ececec',
       scale: 1.8
-    })
+    });
   }
   circle.addEventListener('mouseenter', enterButton, false);
   circle.addEventListener('mouseleave', leaveButton, false);
@@ -49,21 +51,40 @@ arrows.forEach(arrow => {
   anime({
     targets: arrow,
     scale: 1.5
-  })
+  });
   function enterButton() {
       anime({
         targets: arrow,
-        scale: 2
-      })
+        scale: 3
+      });
   }
   function leaveButton() {
     anime({
       delay: 100,
       targets: arrow,
       scale: 1.5
-    })
+    });
   }
 
   arrow.addEventListener('mouseenter', enterButton, false);
   arrow.addEventListener('mouseleave', leaveButton, false);
+})
+
+webLinks.forEach(webLink => {
+  anime({
+    target: webLink,
+    loop: true,
+    scale: 3,
+    backgroundColor: '#26c281'
+  });
+
+  function enterButton() {
+    anime({
+      target: webLink,
+      scale: 3
+    })
+    console.log("SHOULD WORK")
+
+  }
+  webLink.addEventListener('mouseenter', enterButton, false);
 })
